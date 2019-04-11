acb910ca56d157e798fdf3a2feb09119b2a87c39
Dan Fletcher <dannyjfletcher@hotmail.com>
2019-04-11 15:46:50 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Dapper;
+
+namespace voxie_message_seeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
